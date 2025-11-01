6c6f1d5e114acbba879488d87ca4bbfd514ab516
CF8339816 <chris_french_3dga@hotmail.com>
2025-11-01 01:45:21 -0300

    added legend  and setting up the scaling

--- gameProgTxtBsdRPG-Map-ChrisFrench0259182-251110/Program.cs
+++ gameProgTxtBsdRPG-Map-ChrisFrench0259182-251110/Program.cs
@@ -8,8 +8,8 @@ namespace gameProgTxtBsdRPG_Map_ChrisFrench0259182_251110
 {
     internal class Program
     {
-            static char[,] map = new char[,] // dimensions defined by following data:
-    {
+        static char[,] map = new char[,] // dimensions defined by following data:
+{
         {'^','^','^','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`'},
         {'^','^','`','`','`','`','*','*','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','~','~','~','`','`','`'},
         {'^','^','`','`','`','*','*','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','~','~','~','`','`','`','`','`'},
@@ -22,41 +22,119 @@ namespace gameProgTxtBsdRPG_Map_ChrisFrench0259182_251110
         {'`','`','`','`','`','`','`','~','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`'},
         {'`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`'},
         {'`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`'},
-    };
+};
 
-            // usage: map[y, x]
+        // usage: map[y, x]
 
-            // map legend:
-            // ^ = mountain
-            // ` = grass
-            // ~ = water
-            // * = trees
+        // map legend:
+        // ^ = mountain
+        // ` = grass
+        // ~ = water
+        // * = trees
+
+
+
+
 
 
 
         static void Main(string[] args)
         {
 
+
+
+
             DrawMap();
+            mapLegend();
+
+
+            DrawMap(2);
+            mapLegend();
+
+
+            DrawMap(3);
+            mapLegend();
 
 
 
 
 
 
-
-
-
-
-
-
-
+            Console.ForegroundColor = ConsoleColor.White;
 
         }
-        //methods below here
+            //methods below here
 
-        //m1
-        static void DrawMap()
+            //m1
+            static void DrawMap()
+            {
+
+                for (int x = 0; x < map.GetLength(0); x++) // Rows
+                {
+                    for (int y = 0; y < map.GetLength(1); y++) // Columns
+                    {
+
+                        switch (map[x, y]) //changes colours
+                        {
+
+                        case '`': // Grass
+                            Console.ForegroundColor = ConsoleColor.DarkGreen;
+                            break;
+                        case '~': // Water
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            break;
+                        case '^': // Mountain
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                            break;
+                        case '*': // Trees
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            break;
+
+
+                        }
+
+
+
+                        Console.Write(map[x, y]/* + " "*/);
+                    }
+                    Console.WriteLine();
+
+
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                }
+            }
+
+            //m2
+            static void mapLegend()
+            {
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write(" `  ");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Grass");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(" ~  ");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Water");
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(" ^  ");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Mountain");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(" *  ");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Trees");
+
+
+            }
+
+
+        //m3
+        static void DrawMap(int scale)
         {
 
             for (int x = 0; x < map.GetLength(0); x++) // Rows
@@ -66,8 +144,9 @@ namespace gameProgTxtBsdRPG_Map_ChrisFrench0259182_251110
 
                     switch (map[x, y]) //changes colours
                     {
-                        case '-': // Grass
-                            Console.ForegroundColor = ConsoleColor.Green;
+
+                        case '`': // Grass
+                            Console.ForegroundColor = ConsoleColor.DarkGreen;
                             break;
                         case '~': // Water
                             Console.ForegroundColor = ConsoleColor.Blue;
@@ -75,8 +154,8 @@ namespace gameProgTxtBsdRPG_Map_ChrisFrench0259182_251110
                         case '^': // Mountain
                             Console.ForegroundColor = ConsoleColor.Gray;
                             break;
-                        case '*': // Mountain
-                            Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        case '*': // Trees
+                            Console.ForegroundColor = ConsoleColor.Green;
                             break;
 
 
@@ -84,20 +163,19 @@ namespace gameProgTxtBsdRPG_Map_ChrisFrench0259182_251110
 
 
 
-                    Console.Write(map[x, y] + " ");
+                    Console.Write(map[x, y]/* + " "*/);
                 }
                 Console.WriteLine();
 
 
-                Console.ForegroundColor = ConsoleColor.White;
+                Console.ResetColor();
 
             }
         }
-        //m2
 
-        //m3
 
         //m4
+      
 
         //m5
 
@@ -108,5 +186,7 @@ namespace gameProgTxtBsdRPG_Map_ChrisFrench0259182_251110
 
 
 
+
     }
 }
+
